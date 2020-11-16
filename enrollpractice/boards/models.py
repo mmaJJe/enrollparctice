0eedf018686ff2e82f3fd3a0cdbc9c98c034bc1c
duri <bluekmky@likelion.org>
2020-11-16 23:03:01 +0900

    modelUpdate

--- enrollpractice/boards/models.py
+++ enrollpractice/boards/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class EnrollNumber(models.Model):
     number = models.CharField(max_length=4)
+    enrollStartTime = models.ForeignKey(EnrollStartTime, on_delete=models.CASCADE)
 
 class EnrollStartTime(models.Model):
     startTime = models.DateTimeField()
--- enrollpractice/boards/views.py
+++ enrollpractice/boards/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import EnrollNumber, EnrollRank, EnrollStartTime
 
-# Create your views here.
+def waitBoard(request):
+    EnrollStartTimeSet = EnrollStartTime.objects.all()[:1]
+    preEnrollStartTime = EnrollStartTimeSet[1]
+    nextEnrollStartTime = EnrollStartTimeSet[0]
+    nextEnrollNumber = EnrollNumber.objects
+    preEnrollResult = EnrollRank.objects
+    return render(request, "boards/waitboard.html")
+
+def enrollBoard(request):
+    # 신청 하는 경우
+    if request.method == "POST":
+
+    # 신청 페이지로 들어오는 경우
+    else:
+
+    return render(request, "boards/enrollboard.html")
+
+def rankingBoard(request):
+    return render(request, "boards/rankingboard.html")
