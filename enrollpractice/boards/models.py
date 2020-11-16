6cfe147bf91076195e50bcb94d92d5d6cbe1e3c1
duri <bluekmky@likelion.org>
2020-11-16 22:06:37 +0900

    modelCreate

---
+++ enrollpractice/boards/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class EnrollNumber(models.Model):
+    number = models.CharField(max_length=4)
+
+class EnrollStartTime(models.Model):
+    startTime = models.DateTimeField()
+
+class EnrollRank(models.Model):
+    enrollNumber = models.ForeignKey(EnrollNumber, on_delete=models.CASCADE)
+    enrollStartTime = models.ForeignKey(EnrollStartTime, on_delete=models.CASCADE)
+    user = models.CharField(max_length=200)
