8e2558616af51f220989e12a183b148e2efb99e4
mmaJJe <61137472+mmaJJe@users.noreply.github.com>
2020-11-17 11:27:38 +0900

    Update models.py

--- enrollpractice/boards/models.py
+++ enrollpractice/boards/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class EnrollStartTime(models.Model):
+    startTime = models.DateTimeField()
+
 class EnrollNumber(models.Model):
     number = models.CharField(max_length=4)
     enrollStartTime = models.ForeignKey(EnrollStartTime, on_delete=models.CASCADE)
-
-class EnrollStartTime(models.Model):
-    startTime = models.DateTimeField()
 
 class EnrollRank(models.Model):
     enrollNumber = models.ForeignKey(EnrollNumber, on_delete=models.CASCADE)
